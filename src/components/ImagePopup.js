2c66fc9a889489b0c61760914f9b98dfdae2748a
Akimov Eduard <95487679+akimoveduard@users.noreply.github.com>
2022-06-30 11:58:15 +0300

    Update ImagePopup.js

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -22,14 +22,3 @@ function ImagePopup({
 }
 
 export default ImagePopup;
-
-/*    <section className={`popup popup_type_mesto ${isOpen && 'popup_opened'}`} aria-label="Фото места">
-      <div className="mesto">
-        <button className="popup__close" type="button" onClick={onClose}></button>
-        <figure className="mesto__figure">
-          <img className="mesto__image" src={card.link} alt={card.name} />
-          <figcaption className="mesto__caption">{card.name}</figcaption>
-        </figure>
-      </div>
-    </section>
-    */
